437b95432c799ea59688ac07b9e69b782e34d427
hongshulin1605 <1172990087@qq.com>
2016-08-17 17:33:41 +0800

    注释

--- app/src/main/java/com/ouyang/test/MainActivity.java
+++ app/src/main/java/com/ouyang/test/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         Button btn = (Button) findViewById(R.id.btn);
         TextView tv = (TextView) findViewById(R.id.tv);
 
-
+//
         Log.e("print", "onCreate: " + 444444);
         Log.e("print", "onCreate: " + 444444);
         Log.e("print", "onCreate: " + 444444);
