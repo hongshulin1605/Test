5f9ce8c2292419f36ecdc15322f17bba51f15b81
hongshulin1605 <1172990087@qq.com>
2016-08-17 17:25:26 +0800

    log 1111

--- app/src/main/java/com/ouyang/test/MainActivity.java
+++ app/src/main/java/com/ouyang/test/MainActivity.java
@@ -2,6 +2,7 @@ package com.ouyang.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn = (Button) findViewById(R.id.btn);
         TextView tv = (TextView) findViewById(R.id.tv);
+
+        Log.e("print", "onCreate: " + 11111);
+        Log.e("print", "onCreate: " + 11111);
+        Log.e("print", "onCreate: " + 11111);
+        Log.e("print", "onCreate: " + 11111);
+        Log.e("print", "onCreate: " + 11111);
     }
 }
