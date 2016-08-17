3d598ec933b2cee99c1012e3f65878c0ca727939
hongshulin1605 <1172990087@qq.com>
2016-08-17 19:55:25 +0800

    log 7777fff

--- app/src/main/java/com/ouyang/test/MainActivity.java
+++ app/src/main/java/com/ouyang/test/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
         Button btn = (Button) findViewById(R.id.btn);
         TextView tv = (TextView) findViewById(R.id.tv);
-//
+
         Log.e("print", "onCreate: " + 55);
         Log.e("print", "onCreate: " + 55);
         Log.e("print", "onCreate: " + 55);
